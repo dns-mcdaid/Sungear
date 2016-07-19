e57b28de355fcac23d248cf043848e6af7e2a5de
Dennis McDaid <dennis.mcdaid@nyu.edu>
2016-07-19 10:19:31 -0400

    Add event listeners to export list.

--- public/javascripts/gui/exportList.js
+++ public/javascripts/gui/exportList.js
@@ -1,6 +1,23 @@
 function ExportList(g, context) {
     this.genes = g; /** {GeneList} */
-    this.context = context;
+    this.context = context; /** {AppletContext} */
     this.localUpdate = false;
-    // TODO: @Dennis finish.
+    this.groupCnt = 0;
+    this.table = document.getElementById('controlFtable');
+    this.model = new ExportModel();
+    this.removeB = document.getElementById('removeB');
+    this.exportB = document.getElementById('exportB');
+    this.removeB.addEventListener("click", function() {
+        this.model.deleteSelected();
+    });
+    this.exportB.addEventListener("click", function() {
+        console.log("List size: " + this.model.getList().length);
+        for (var i = 0; i < this.model.getList().length; i++) {
+            var l = this.model.getList()[i];
+            console.log(l.name + " (" + l.genes.length + ")"); // TODO: @Dennis check back to make sure it should be genes.length
+        }
+        this.exportGeneList();
+    });
 }
+
+module.exports = ExportList;
