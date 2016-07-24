e547458aa223bcce2247433e7e93055017991d89
Dennis McDaid <dennis.mcdaid@nyu.edu>
2016-07-24 18:44:53 -0400

    Commenting out broken code and adding out.js to the .gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-// var dataTest = require('../public/javascripts/test/dataTest');
-// var geneTest = require('../public/javascripts/test/geneTest');
-var hyperTest = require('../public/javascripts/test/HyperGeoTest');
+// var hyperTest = require('../public/javascripts/test/HyperGeoTest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    hyperTest.test();
+    // hyperTest.test();
     res.render('index', { title: 'Sungear' });
 });
 
